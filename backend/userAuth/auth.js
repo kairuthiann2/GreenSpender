6cab9bc9e835ae724314b0ace126581e7aee97a9
Ann <kairuthiann2@gmail.com>
2024-09-05 15:59:55 +0300

    Added in userAuth folder With auth.js file for generating JWT token and user authentication

---
+++ backend/userAuth/auth.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const secretKey = process.env.JWT_SECRET;
+//console.log('Secrete Key:', secretKey);
+
+// Generate JWT token
+const generateToken = (user) => {
+    return jwt.sign({ id: user.id, email: user.email }, secretKey, { expiresIn: '1h' });
+};
+
+// Authentication middleware 
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader?.startsWith("Bearer ")) return res.sendStatus(401); // Unauthorized
+
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, secretKey, (err, user) => {
+        if (err) { 
+            //console.log('Token:', token);
+            console.log('JWT Verification Error:', err.message);
+            return res.sendStatus(403); // Forbidden
+
+        } 
+        req.user = user;
+        next();
+    
+    });
+};
+
+module.exports = { generateToken, authenticateToken };

0ef41e413d01ace40b797a2657c6af74290f41b5
Ann <kairuthiann2@gmail.com>
2024-09-10 15:16:15 +0300

    Imported missing authentication middleware from userAuth Fixed incorrect importation of authentication middleware

--- backend/routes/metricsRoute.js
+++ backend/routes/metricsRoute.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const db = require('../db');
 const { calculateImpactMetrics } = require('../controllers/metricsControllers');
+const { authenticateToken } = require('../userAuth/auth');
 
 // Create an instance of express
 const router = express.Router();
 
+// Applying the authenticate middleware
+router.use(authenticateToken);
+
 router.get('/', async (req, res) => {
     try {
+        // console.log(req.user);
         const userId = req.user.id;
         const metrics = await calculateImpactMetrics(userId);
         res.json(metrics);
         
     } catch (error) {
-        res.status(500).json('Failed to fetch metrics data', error);
+        console.error('Error fetching metrics data:', error);
+        res.status(500).json({ message: 'Failed to fetch metrics data', error });
     }
 });
 

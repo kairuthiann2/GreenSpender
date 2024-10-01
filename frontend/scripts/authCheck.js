b45217495041ab94ac50250c85569c189ba50ad1
Ann <kairuthiann2@gmail.com>
2024-10-01 16:16:54 +0300

    used the function for token valiadation to check if the token is valid befor granting access to the view expense dashboard

--- frontend/scripts/authCheck.js
+++ frontend/scripts/authCheck.js
@@ -2,18 +2,29 @@ document.addEventListener('DOMContentLoaded', () => {
     const jwtToken = localStorage.getItem('token');
     const getStartedButton = document.querySelector('.bt a');
     const loginLink = document.querySelector('.nav-links a[href="login.html"]');
-    const registerLink = document.querySelector('.nav-links a[href="register.html]');
+    const registerLink = document.querySelector('.nav-links a[href="register.html"]');
 
     // check if JWT token exist
-    if (!jwtToken) {
+    if (!jwtToken || !isTokenValid(jwtToken)) {
         getStartedButton.href = 'register.html'; // Redirect to register form
+
+       // getStartedButton.style.display = 'none'; // hide button for unauthenticated users
 
     } else {
         // If the token exist 
         getStartedButton.href = 'view_expense.html'; // Redirect to view expense dashboard page
     };
 
-    // Hide login and register for logged in users
-    loginLink.style.display = 'none';
-    registerLink.style.display = 'none';
+    /* Hide login and register for logged in users
+    if (loginLink) {
+
+        loginLink.style.display = 'none';
+    }
+    
+    if (registerLink) {
+
+        registerLink.style.display = 'none';
+    }*/
+    
+    
 });

34f98135fd413e251a135acad1a4ade5e4dab454
Ann <kairuthiann2@gmail.com>
2024-10-07 15:49:21 +0300

    Excludedthe Authorization header in the logout request withing the helper function

--- frontend/scripts/apiCalls.js
+++ frontend/scripts/apiCalls.js
@@ -1,28 +1,34 @@
 // Helper function for making API  calls
- const apiCall = async (url, method, body = null) => {
-    const token = localStorage.getItem('token');
-    const options = {
-        method: method,
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`
-        },
-        body: body ? JSON.stringify(body) : null
-    };
-    console.log(`Making ${method} request to ${url} with body:`, body);
+const apiCall = async (url, method, body = null) => {
+  const token = localStorage.getItem("token");
+  const options = {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: body ? JSON.stringify(body) : null,
+  };
 
-    const response = await fetch(url, options);
-
-    console.log(`Response status: ${response.status}`)
-
-    if (!response.ok) {
-        console.log(`Failed to ${method} data. Status: ${response.status} `)
-        throw new Error(`Failed to ${method} data`);
-
+  // Only add the token if it's not the logout route
+  if (url !== "/api/v1/logout") {
+    const token = localStorage.getItem("token");
+    if (token) {
+      options.headers["Authorization"] = `Bearer ${token}`;
     }
+  }
 
-    console.log(`${method} request to ${url} was successful.`)
-    return await response.json();
+  console.log(`Making ${method} request to ${url} with body:`, body);
 
+  const response = await fetch(url, options);
+
+  console.log(`Response status: ${response.status}`);
+
+  if (!response.ok) {
+    console.log(`Failed to ${method} data. Status: ${response.status} `);
+    throw new Error(`Failed to ${method} data`);
+  }
+
+  console.log(`${method} request to ${url} was successful.`);
+  return await response.json();
 };
-

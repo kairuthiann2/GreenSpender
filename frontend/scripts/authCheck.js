399e910917b12dd785bb11a40db684cbb8566461
Ann <kairuthiann2@gmail.com>
2024-10-08 11:28:55 +0300

    Cleared excesive spacing Deleted unnecessary logs

--- frontend/scripts/authCheck.js
+++ frontend/scripts/authCheck.js
@@ -1,30 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const jwtToken = localStorage.getItem('token');
-    const getStartedButton = document.querySelector('.bt a');
-    const loginLink = document.querySelector('.nav-links a[href="login.html"]');
-    const registerLink = document.querySelector('.nav-links a[href="register.html"]');
+// check if the token is valid before redirectig to user registration or view expense
+document.addEventListener("DOMContentLoaded", () => {
+  const jwtToken = localStorage.getItem("token");
+  const getStartedButton = document.querySelector(".bt a");
 
-    // check if JWT token exist
-    if (!jwtToken || !isTokenValid(jwtToken)) {
-        getStartedButton.href = 'register.html'; // Redirect to register form
-
-       // getStartedButton.style.display = 'none'; // hide button for unauthenticated users
-
-    } else {
-        // If the token exist 
-        getStartedButton.href = 'view_expense.html'; // Redirect to view expense dashboard page
-    };
-
-    /* Hide login and register for logged in users
-    if (loginLink) {
-
-        loginLink.style.display = 'none';
-    }
-    
-    if (registerLink) {
-
-        registerLink.style.display = 'none';
-    }*/
-    
-    
+  // check if JWT token exist
+  if (!jwtToken || !isTokenValid(jwtToken)) {
+    getStartedButton.href = "register.html"; // Redirect to register form
+  } else {
+    // If the token exist
+    getStartedButton.href = "view_expense.html"; // Redirect to view expense dashboard page
+  }
 });

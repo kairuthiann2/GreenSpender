7cc1a40ae06f7ae5e3503e28186cc2698c68a548
Ann <kairuthiann2@gmail.com>
2024-10-08 11:51:37 +0300

    Updated the login and registered urls with the versioned urls routes

--- frontend/scripts/forms.js
+++ frontend/scripts/forms.js
@@ -1,95 +1,88 @@
- //import { apiCall } from "./apiCalls";
+// Login and Register form validation and logic
+document.addEventListener("DOMContentLoaded", () => {
+  const registerForm = document.getElementById("register-form");
+  const loginForm = document.getElementById("login-form");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const registerForm = document.getElementById("register-form");
-    const loginForm = document.getElementById("login-form");
+  // Check if registerForm exists before adding event listener
+  if (registerForm) {
+    registerForm.addEventListener("submit", async (e) => {
+      e.preventDefault();
 
-   
-    // Check if registerForm exists before adding event listener
-    if (registerForm) {
-        // User register form submission
-        registerForm.addEventListener("submit", async (e) => {
-        // Prevent the default form submission behavior
-        e.preventDefault();
-        console.log('Form submission prevented');
+      console.log("Form submission prevented");
 
-        // Retrieve values from the input fields
-        const username = document.getElementById("username").value;
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-        //console.log('Input Values:', { username, email, password });
+      // Retrieve values from the input fields
+      const username = document.getElementById("username").value;
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      //console.log('Input Values:', { username, email, password });
 
-        try {
-            // Send a POST request to the server with the registration data
-            console.log('Sending registration request to server...');
-            const response = await apiCall('/api/register', 'POST', { username, email, password });
-
-
-            // If registration is successful, notify the user
-            console.log('Registration succesfull', response);
-            alert('Registration successful');
-
-            // The redirect the user to the login page
-            window.location.href = './pages/login.html';
-
-        } catch (error) {
-            // Handle any errors that occur during the fetch request
-            console.error('Error registering:', error.message);
-            alert('Error registering. please try again');
-        }
+      try {
+        // Send a POST request to the server with the registration data
+        console.log("Sending registration request to server...");
+        const response = await apiCall("/api/v1/register", "POST", {
+          username,
+          email,
+          password,
         });
-       // display error if registerForm is not found 
-    } else {
-        console.error("Register form not found");
 
-    }
-    
+        // If registration is successful, notify the user
+        console.log("Registration succesfull", response);
+        alert("Registration successful");
 
-    // Check if loginForm exists before adding event listener
-    if (loginForm) {
-        // User login form submission
-         loginForm.addEventListener('submit', async (e) => {
-             e.preventDefault();
-             
+        // The redirect the user to the login page
+        window.location.href = "./pages/login.html";
+      } catch (error) {
+        // Handle any errors that occur during the fetch request
+        console.error("Error registering:", error.message);
+        alert("Error registering. please try again");
+      }
+    });
+    // display error if registerForm is not found
+  } else {
+    console.error("Register form not found");
+  }
 
-        // Retrieve values from input field
-        const email = document.getElementById("email").value;
-        const password = document.getElementById("password").value;
-        //console.log('Input Values:', { email, password });
+  // Check if loginForm exists before adding event listener
+  if (loginForm) {
+    // User login form submission
+    loginForm.addEventListener("submit", async (e) => {
+      e.preventDefault();
 
-        try {
+      // Retrieve values from input field
+      const email = document.getElementById("email").value;
+      const password = document.getElementById("password").value;
+      //console.log('Input Values:', { email, password });
 
-            // Send a POST request to the server with the login data           
-            const result = await apiCall('/api/login', 'POST', { email, password });
-                
-           
-            // If login was successful notify the user
-            console.log('Login was successful');
-            alert('Login was successful');
+      try {
+        // Send a POST request to the server with the login data
+        const result = await apiCall("/api/v1/login", "POST", {
+          email,
+          password,
+        });
 
-            // Log and save the received token
-            const token = result.token;
-            localStorage.setItem('token', token);
+        // If login was successful notify the user
+        console.log("Login was successful");
+        alert("Login was successful");
 
-            // Decode the token to extract the user ID
-            const decodeToken = jwt_decode(token);
-            localStorage.setItem('user_id', decodeToken.id);
+        // Log and save the received token
+        const token = result.token;
+        localStorage.setItem("token", token);
 
-            // Then redirect the user to get started
-            window.location.href = '/pages/view_expense.html';
-          
-        } catch (error) {
-            // Handle any error that occur during the fetch request 
-            console.error("Error login:", error);
-            alert('Error Login. please try again later');
-        }
+        // Decode the token to extract the user ID
+        const decodeToken = jwt_decode(token);
+        localStorage.setItem("user_id", decodeToken.id);
 
+        // Then redirect the user to get started
+        window.location.href = "/pages/view_expense.html";
+      } catch (error) {
+        // Handle any error that occur during the fetch request
+        console.error("Error login:", error);
+        alert("Error Login. please try again later");
+      }
     });
 
-        // Display error if loginForm is not found 
-    } else {
-        console.log("Login form not found")
-
-    }
-
+    // Display error if loginForm is not found
+  } else {
+    console.log("Login form not found");
+  }
 });

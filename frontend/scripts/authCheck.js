75b0fcd1cf0519fbc7b128c606d6743aa1204e9a
Ann <kairuthiann2@gmail.com>
2024-10-09 11:02:52 +0300

    Include the logic to check if the the token is present and still valid after login If so prompt the user with alert message to login once more

--- frontend/scripts/authCheck.js
+++ frontend/scripts/authCheck.js
@@ -1,13 +1,20 @@
-// check if the token is valid before redirectig to user registration or view expense
+// check if the token is valid on Get started button before redirectig to user registration or view expense
 document.addEventListener("DOMContentLoaded", () => {
   const jwtToken = localStorage.getItem("token");
   const getStartedButton = document.querySelector(".bt a");
 
-  // check if JWT token exist
-  if (!jwtToken || !isTokenValid(jwtToken)) {
-    getStartedButton.href = "register.html"; // Redirect to register form
+  // Token validation check before getting started
+  if (jwtToken && isTokenValid(jwtToken)) {
+    getStartedButton.href = "view_expense.html"; // Redirect to view expenses
   } else {
-    // If the token exist
-    getStartedButton.href = "view_expense.html"; // Redirect to view expense dashboard page
+    // If the token does not exist
+    getStartedButton.href = "register.html"; // Redirect to register form
+  }
+
+  // token expiration after login
+  if (!jwtToken || !isTokenValid(jwtToken)) {
+    alert("You are not logged in. Please log in again.");
+    localStorage.removeItem("token");
+    window.location.href = "/login.html";
   }
 });

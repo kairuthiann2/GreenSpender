220c5a227511e5acfb661a4c708c9cd19b0a44f0
Ann <kairuthiann2@gmail.com>
2024-10-29 16:32:24 +0300

    Updated the code to import bcryptjs instead of bcrypt

--- backend/routes/loginRoute.js
+++ backend/routes/loginRoute.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const express = require("express");
 const db = require("../db");
 const { generateToken } = require("../userAuth/auth");
